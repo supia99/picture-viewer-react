a6d580515f249109fa2092ecbdb5b08f3f534876
supia99 <3kzk9.vl.embox@gmai.com>
2023-04-13 00:17:04 +0900

    feat: キーボード操作

--- src/module/doujinnomori/doujinnomori.tsx
+++ src/module/doujinnomori/doujinnomori.tsx
@@ -5,10 +5,15 @@ import "./doujinnomori.css";
 import { DmLink } from "../doujinnomoriComponent/link";
 import { saveDoujinnomori } from "../../api/saveDoujinnomori";
 import { escapeURI } from "../../util/escapeURI";
+import { dmNavigate } from "../doujinnomoriComponent/navigate";
+import { useNavigate } from "react-router-dom";
 // import { Link } from "react-router-dom";
 
 export const Doujinnomori = ({ path }: { path: string }) => {
   const [dmInfos, setDmInfos] = useState([] as DoujinnmoriObject[]);
+  const navigate = useNavigate();
+  const dmModuleRef = useRef(null);
+
   console.log(`path: ${path}`);
   useEffect(() => {
     (async () => {
@@ -21,7 +26,40 @@ export const Doujinnomori = ({ path }: { path: string }) => {
       // setDmInfos(await mutiTimeGetRequest({from: relayGetCount, to: toRelayGetCount}))
       // setRelayGetCount(toRelayGetCount)
     })();
+    (dmModuleRef.current! as any).focus();
   }, [path]);
+
+  const keyOperation = (e: any) => {
+    console.log(`e.key: ${e.key}`);
+    switch (e.key) {
+      case "ArrowLeft":
+        // TODO: 戻る
+        dmNavigate(
+          navigate,
+          path ? "/" + (Number(path.replace("/", "")) - 1) : "/1"
+        );
+        break;
+      case "ArrowRight":
+        // TODO: すすむ
+        dmNavigate(
+          navigate,
+          path ? "/" + (Number(path.replace("/", "")) + 1) : "/2"
+        );
+        break;
+      case "ArrowUp":
+        // TODO: 10戻る
+        const subNum = Number(path.replace("/", "")) - 10;
+        dmNavigate(navigate, path && subNum > 0 ? "/" + subNum : "");
+        break;
+      case "ArrowDown":
+        // TODO: 10進む
+        dmNavigate(
+          navigate,
+          path ? "/" +( Number(path.replace("/", "")) + 10) : ""
+        );
+        break;
+    }
+  };
 
   dmInfos.length && console.log(`lenght: ${dmInfos.length}`);
   return (
@@ -29,17 +67,41 @@ export const Doujinnomori = ({ path }: { path: string }) => {
       <DmLink
         to={path ? "/" + (Number(path.replace("/", "")) - 1) : "/1"}
         className="prev"
-      >＜</DmLink>
-      <ul className="comic-card-list">
+      >
+        ＜
+      </DmLink>
+      <ul
+        className="comic-card-list"
+        onKeyDown={(e) => keyOperation(e)}
+        tabIndex={0}
+        ref={dmModuleRef}
+      >
         {dmInfos.map((dmInfo) => (
           <li className="comic-card" key={dmInfo.id}>
-          <p className={new Date(dmInfo.modified).getFullYear() - new Date(dmInfo.created).getFullYear() > 0? "comic-createdAt-old": "comic-createdAt"}>
+            <p
+              className={
+                new Date(dmInfo.modified).getFullYear() -
+                  new Date(dmInfo.created).getFullYear() >
+                0
+                  ? "comic-createdAt-old"
+                  : "comic-createdAt"
+              }
+            >
               createdAt: {new Date(dmInfo.created).toLocaleDateString()}
             </p>
             {/* <p className="comic-createdAt">
               {new Date(dmInfo.modified).toLocaleDateString()}
             </p> */}
-            <img src={dmInfo.thumbnailUrl} className="comic-thumbnail" onClick={() => {saveDoujinnomori({uuid: escapeURI(dmInfo.uuid), title: escapeURI(dmInfo.name)})}}/>
+            <img
+              src={dmInfo.thumbnailUrl}
+              className="comic-thumbnail"
+              onClick={() => {
+                saveDoujinnomori({
+                  uuid: escapeURI(dmInfo.uuid),
+                  title: escapeURI(dmInfo.name),
+                });
+              }}
+            />
             {/* TODO: タイトルを押したときにタブが開き、検索がされる */}
             <p className="comic-name">{dmInfo.name}</p>
           </li>
@@ -48,7 +110,9 @@ export const Doujinnomori = ({ path }: { path: string }) => {
       <DmLink
         className="next"
         to={path ? "/" + (Number(path.replace("/", "")) + 1) : "/2"}
-      >＞</DmLink>
+      >
+        ＞
+      </DmLink>
     </div>
   );
 };
