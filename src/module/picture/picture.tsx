d4c7de4b28aaf01bb3297c3bfcf263b2347ebc69
supia99 <3kzk9.vl.embox@gmai.com>
2023-06-25 13:08:18 +0900

    chore: anchorに含まれる.を_に置換

--- src/module/picture/picture.tsx
+++ src/module/picture/picture.tsx
@@ -58,7 +58,6 @@ export const Picture = ({
         firstOrlastPage === "first" ? 0 : files.length - 1
       }`
     );
-    // setFileNo(firstOrlastPage === "first" ? 0 : files.length - 1);
   }, [files]);
 
   const nextDirectory = () => {
@@ -69,6 +68,42 @@ export const Picture = ({
     setFirstOrlastPage("last");
     pictureNavigate(navigate, prevDirectoryPath);
   };
+  // const nextFile = () => {
+  //   const anchors = location.href.match(/#.+/);
+  //   if (anchors) {
+  //     let i = 0;
+  //     const anchor = anchors[0].replace("#", "");
+  //     while (files[i].name.replace(".", "_") !== anchor) {
+  //       i++;
+  //     }
+  //     if (i + 1 > files.length) {
+  //       nextDirectory();
+  //       return;
+  //     }
+  //     pictureNavigate(navigate, directoryPath+"#"+files[i + 1].name.replace(".", "_"));
+  //   } else {
+  //     if (files.length > 2) {
+  //       pictureNavigate(navigate, directoryPath+"#"+files[2].name.replace(".", "_"));
+  //       return;
+  //     }
+  //     nextDirectory();
+  //   }
+  // };
+  // const prevFile = () => {
+  //   const anchors = location.href.match(/#.+/);
+  //   if (anchors) {
+  //     let i = 0;
+  //     const anchor = anchors[0].replace("#", "");
+  //     while (files[i].name.replace(".", "_") !== anchor) {
+  //       i++;
+  //     }
+  //     if (i !== 0) {
+  //       pictureNavigate(navigate, directoryPath+"#"+files[i-1].name.replace(".", "_"))
+  //       return;
+  //     }
+  //   } 
+  //   prevDirectory();
+  // };
 
   const onOffSlideShow = () => {
     console.log("run slideShow");
@@ -91,10 +126,10 @@ export const Picture = ({
     console.log(`e.key: ${e.key}`);
     switch (e.key) {
       case "ArrowLeft":
-        // TODO: prevFile();
+        // prevFile();
         break;
       case "ArrowRight":
-        // TODO: nextFile();
+        // nextFile();
         break;
       case "ArrowUp":
         prevDirectory();
@@ -120,11 +155,11 @@ export const Picture = ({
         return (
           <div
             className="navigateArea"
-            style={
-            createImageStyle(`http://${fileDomain}${directoryPath}/${file.name}`)
-          }
+            style={createImageStyle(
+              `http://${fileDomain}${directoryPath}/${file.name}`
+            )}
             tabIndex={i}
-            id={`${file.name}`}
+            id={`${file.name.replace(".", "_")}`}
             key={`${file.name}`}
           >
             {i === 0 ? (
@@ -136,7 +171,7 @@ export const Picture = ({
             ) : (
               <a
                 className="prevArea"
-                href={changeHrefAnchor(`${files[i - 1].name}`)}
+                href={changeHrefAnchor(`${files[i - 1].name.replace(".", "_")}`)}
               ></a>
             )}
             <div className="directoryArea">
@@ -167,7 +202,7 @@ export const Picture = ({
             ) : (
               <a
                 className="nextArea"
-                href={changeHrefAnchor(`${files[i + 1].name}`)}
+                href={changeHrefAnchor(`${files[i + 1].name.replace(".", "_")}`)}
               ></a>
             )}
           </div>
