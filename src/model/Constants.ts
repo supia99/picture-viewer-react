d23cd4e43446271e8d0fb63c79c236db134eb9c1
supia99 <3kzk9.vl.embox@gmai.com>
2024-03-09 23:23:17 +0900

    feat: menuをアイコン追加と並び替え順序を選択できる

---
+++ src/model/Constants.ts
@@ -0,0 +1,7 @@
+export const SORT_BY_KEY_NAME = "sortBy"
+export enum SortType {
+  NAME = "NAME",
+  NAME_D ="NAME_D",
+  TIME = "TIME",
+  TIME_D = "TIME_D"
+}
--- src/module/menu/menu.tsx
+++ src/module/menu/menu.tsx
@@ -1,30 +1,44 @@
-import { Dispatch, useContext, useState } from "react";
+import { Dispatch, useContext, useEffect, useState } from "react";
 import { SlideWaitTimeContext } from "../../PictureApp";
 import { Link } from "react-router-dom";
 import "./menu.css";
+import { SORT_BY_KEY_NAME, SortType } from "../../model/Constants";
 
 type props = {
   setSlideWaitTime: Dispatch<React.SetStateAction<number>>;
 };
+
 export const Menu = ({ setSlideWaitTime }: props) => {
   const [isShowMenu, setIsShowMenu] = useState(true);
   const slideShowTime = useContext(SlideWaitTimeContext);
+  const [selectSortByState, setSortByState] = useState("");
+
+  useEffect(() => {
+    const stateBy = getSortBy() || SortType.TIME_D;
+    setSortBy(stateBy);
+    setSortByState(stateBy);
+  }, []);
+
   return (
     <div className="menu">
-      {/* <Link to="/">home</Link> */}
-      {/* {slideShowTime} */}
       {isShowMenu && (
         <div className="content">
-          <Link to="/">home</Link>
+          <div className="icons">
+          <Link to="/" className="home">
+            <img src="public/home.svg"></img>
+          </Link>
           <a
+            className="return"
             onClick={() => {
               location.href = returnOneHigherPage(location.href);
             }}
           >
-            戻る
+            <img src="public/return.svg"></img>
           </a>
+          </div>
+          <div className="options">
           <div className="slideShowTimeArea">
-            <p>slide show time(s):</p>
+            <label className="slideShowLable">slide show time(s):</label>
             <input
               type="number"
               value={slideShowTime}
@@ -34,14 +48,36 @@ export const Menu = ({ setSlideWaitTime }: props) => {
               className="slideShowTimeInput"
             />
           </div>
+          <select
+            value={selectSortByState}
+            onChange={(e) => {
+              setSortBy(e.target.value);
+              setSortByState(e.target.value);
+            }}
+          >
+            <option value={SortType.TIME_D}>日付 降順</option>
+            <option value={SortType.NAME_D}>名前 降順</option>
+          </select>
+          </div>
         </div>
       )}
-      {/* TODO: 上のディレクトリに移動できる */}
-      <div className="tab" onClick={() => setIsShowMenu(!isShowMenu)}></div>
+      <div
+        className="tab"
+        onClick={() => {
+          setIsShowMenu(!isShowMenu);
+        }}
+      ></div>
     </div>
   );
 };
 
 const returnOneHigherPage = (path: string) => {
   return path.substring(0, path.lastIndexOf("/"));
+};
+
+const setSortBy = (value: string) => {
+  localStorage.setItem(SORT_BY_KEY_NAME, value);
+};
+const getSortBy = () => {
+  return localStorage.getItem(SORT_BY_KEY_NAME);
 };
