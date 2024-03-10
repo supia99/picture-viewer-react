2b476635ffe76f47b5736df1337ef9108c7e6159
supia99 <3kzk9.vl.embox@gmai.com>
2024-03-10 20:24:24 +0900

    chore: menu tabでアイコンを使用する

--- src/module/menu/menu.tsx
+++ src/module/menu/menu.tsx
@@ -24,49 +24,50 @@ export const Menu = ({ setSlideWaitTime }: props) => {
       {isShowMenu && (
         <div className="content">
           <div className="icons">
-          <Link to="/" className="home">
-            <img src="/home.svg"></img>
-          </Link>
-          <a
-            className="return"
-            onClick={() => {
-              location.href = returnOneHigherPage(location.href);
-            }}
-          >
-            <img src="/return.svg"></img>
-          </a>
+            <Link to="/" className="home">
+              <img src="/home.svg"></img>
+            </Link>
+            <a
+              className="return"
+              onClick={() => {
+                location.href = returnOneHigherPage(location.href);
+              }}
+            >
+              <img src="/return.svg"></img>
+            </a>
           </div>
           <div className="options">
-          <div className="slideShowTimeArea">
-            <label className="slideShowLable">slide show time(s):</label>
-            <input
-              type="number"
-              value={slideShowTime}
-              onChange={(e) =>
-                setSlideWaitTime(Number.parseInt(e.target.value))
-              }
-              className="slideShowTimeInput"
-            />
-          </div>
-          <select
-            value={selectSortByState}
-            onChange={(e) => {
-              setSortBy(e.target.value);
-              setSortByState(e.target.value);
-            }}
-          >
-            <option value={SortType.TIME_D}>日付 降順</option>
-            <option value={SortType.NAME_D}>名前 降順</option>
-          </select>
+            <div className="slideShowTimeArea">
+              <label className="slideShowLable">slide show time(s):</label>
+              <input
+                type="number"
+                value={slideShowTime}
+                onChange={(e) =>
+                  setSlideWaitTime(Number.parseInt(e.target.value))
+                }
+                className="slideShowTimeInput"
+              />
+            </div>
+            <select
+              value={selectSortByState}
+              onChange={(e) => {
+                setSortBy(e.target.value);
+                setSortByState(e.target.value);
+              }}
+            >
+              <option value={SortType.TIME_D}>日付 降順</option>
+              <option value={SortType.NAME_D}>名前 降順</option>
+            </select>
           </div>
         </div>
       )}
-      <div
+      <img
+        src={isShowMenu ? "/up.svg" : "/down.svg"}
         className="tab"
         onClick={() => {
           setIsShowMenu(!isShowMenu);
         }}
-      ></div>
+      ></img>
     </div>
   );
 };
