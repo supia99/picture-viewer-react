ba6f336a5a5e78a29b57d2b7970bf29907d92b76
supia99 <3kzk9.vl.embox@gmai.com>
2025-10-16 00:56:16 +0900

    feat: 画像がブラウザの横幅に合わせて、横にならず数が変わる

--- src/module/wnacg/wnacg.tsx
+++ src/module/wnacg/wnacg.tsx
@@ -1,29 +1,32 @@
 import { useEffect, useState } from "react";
 import { WnacgObject } from "../../model/WnacgObject";
 import axios from "axios";
+import "./wnacg.css";
 
-
-const url = "http://192.168.50.134:8081/html/wnacg"
+const url = "http://192.168.50.134:8081/html/wnacg";
 
 export const WNACG = () => {
   const [datas, setDatas] = useState([] as WnacgObject[]);
   useEffect(() => {
     (async () => {
-      const response = (await axios.get(url)) as  {data: WnacgObject[]};
+      const response = (await axios.get(url)) as { data: WnacgObject[] };
       setDatas(response.data);
-      console.log(response.data)
+      console.log(response.data);
     })();
   }, []);
 
   return (
-    <>
-      {/* {datas} */}
+    <ul className="list">
       {datas.map((data, index) => (
-        <div key={index}>
-          <h3>{data.title}</h3>
-          <img src={`https:${data.thumbnailUrl}`} alt={data.title} />
-        </div>
+        <li key={index} className="list-item">
+          {/* TODO: aタグが画像を囲む */}
+          {/* TODO: aタグの遷移先がない */}
+          <a href={data.href} className="link">
+            <img src={`https:${data.thumbnailUrl}`} alt={data.title} />
+            <p className="title">{data.title}</p>
+          </a>
+        </li>
       ))}
-    </>
-  )
+    </ul>
+  );
 };
