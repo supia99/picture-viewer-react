6c8a22b7a6c42375dc152779ef6a08137e0a87ee
supia99 <3kzk9.vl.embox@gmai.com>
2025-10-19 23:24:08 +0900

    del: 不要にimport

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PictureApp from "./PictureApp";
 import ErrorPage from "./error-page";
 import { Home } from "./module/home/home";
-import DoujinnomoriApp from "./DoujinnomoriApp";
 import WNACGApp from "./WNACGApp";
 
 // react-router-dom https://reactrouter.com/en/main/start/tutorial
